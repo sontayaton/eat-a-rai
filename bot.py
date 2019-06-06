838b306780c66f58ce7feac9e739f51fa81baa4d
Sontaya Sritorn <sontaya.sri@gmail.com>
2019-06-06 17:10:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ def webhook():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if any(event.message.text in match_messages):
+    if event.message.text in match_messages:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=random.choice(reply_lists)))
