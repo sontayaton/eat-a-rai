9321840402e96683caeac302ae38c7e066cdf70b
Sontaya Sritorn <sontaya.sri@gmail.com>
2019-06-06 15:52:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
@@ -5,8 +6,8 @@ from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN',""))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET',""))
 
 @app.route("/")
 def hello():
